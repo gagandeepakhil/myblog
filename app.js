97e9b8e4181151295bab94d283ea2fd863b24390
GAGAN DEEP <gagandeepdunna9@gmail.com>
2023-01-07 23:41:45 +0530

    Update app.js

--- app.js
+++ app.js
@@ -27,59 +27,59 @@ app.use((req, res, next) => {
 });
 
 // routes
-app.get(__dir+'/', (req, res) => {
-  res.redirect(__dir+'views/blogs');
+app.get('/', (req, res) => {
+  res.redirect('/blogs');
 });
 
-app.get(__dir+'/about', (req, res) => {
-  res.render(__dir+'about', { title: 'About' });
+app.get('/about', (req, res) => {
+  res.render('about', { title: 'About' });
 });
 
 // blog routes
-app.get(__dir+'/blogs/create', (req, res) => {
-  res.render(__dir+'create', { title: 'Create a new blog' });
+app.get('/blogs/create', (req, res) => {
+  res.render('create', { title: 'Create a new blog' });
 });
 
-app.get(__dir+'/blogs', (req, res) => {
+app.get('/blogs', (req, res) => {
   Blog.find().sort({ createdAt: -1 })
     .then(result => {
-      res.render(__dir+'index', { blogs: result, title: 'All blogs' });
+      res.render('index', { blogs: result, title: 'All blogs' });
     })
     .catch(err => {
       console.log(err);
     });
 });
 
-app.post(__dir+'/blogs', (req, res) => {
+app.post('/blogs', (req, res) => {
   // console.log(req.body);
   const blog = new Blog(req.body);
 
   blog.save()
     .then(result => {
-      res.redirect(__dir+'/blogs');
+      res.redirect('/blogs');
     })
     .catch(err => {
       console.log(err);
     });
 });
 
-app.get(__dir+'/blogs/:id', (req, res) => {
+app.get('/blogs/:id', (req, res) => {
   const id = req.params.id;
   Blog.findById(id)
     .then(result => {
-      res.render(__dir+'details', { blog: result, title: 'Blog Details' });
+      res.render('details', { blog: result, title: 'Blog Details' });
     })
     .catch(err => {
       console.log(err);
     });
 });
 
-app.delete(__dir+'/blogs/:id', (req, res) => {
+app.delete('/blogs/:id', (req, res) => {
   const id = req.params.id;
   
   Blog.findByIdAndDelete(id)
     .then(result => {
-      res.json({ redirect:__dir+'/blogs' });
+      res.json({ redirect: '/blogs' });
     })
     .catch(err => {
       console.log(err);
@@ -88,6 +88,6 @@ app.delete(__dir+'/blogs/:id', (req, res) => {
 
 // 404 page
 app.use((req, res) => {
-  res.status(404).render(__dir+'404', { title: '404' });
+  res.status(404).render('404', { title: '404' });
 });
 module.exports = app
